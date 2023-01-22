8de7407deaedc3c769e5371c1ab3c555663ac668
«Sergei» <seregavil@mail.ru>
2023-01-22 16:40:53 +0300

    программа показывает сколько чисел строго больше 0 ввел пользователь

--- Задача017/Program.cs
+++ Задача017/Program.cs
@@ -5,8 +5,8 @@
 
 Console.Write("Введите числа(через пробел): ");
 int[] array = Array.ConvertAll(Console.ReadLine()!.Split(" "), int.Parse);
+
 int count = 0;
- 
 for (int i = 0; i < array.Length; i++)
 {
     if (array[i] > 0)
