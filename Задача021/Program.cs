7ac0d7de768862d942e634b46000af49c3fde461
«Sergei» <seregavil@mail.ru>
2023-01-23 22:20:53 +0300

    программа выдает двумерный массив из целых чисел и находит средне арифметическое элементо в каждом столбце

---
+++ Задача021/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите количество строк: ");
+int rown = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rown, columns, 1, 20);
+PrintArray(array);
+
+System.Console.WriteLine();
+
+PrintArray2(array);
+
+
+// ---Методы---
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+// ---выводит двумерный массив в консоль---
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// ---считает среднеарифметическое по столбцам---
+void PrintArray2(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        double res = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+            res =sum /array.GetLength(0);
+        }
+        Console.Write($"{Math.Round(res,2)} ");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
